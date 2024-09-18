d5df9bfb0a50a69c0bd0e3594c93afc078eaed10
Joepriestley <joeamonoo1114@gmail.com>
2024-09-18 23:48:42 +0100

    Grading scheme

---
+++ exo2.js
@@ -0,0 +1,44 @@
+/*Step 2Passed
+Now the teacher needs your help converting the student score to a letter grade.
+
+Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+
+Here are the scores and their corresponding letter grades:
+
+Score Range	Grade
+100	"A++"
+90 - 99	"A"
+80 - 89	"B"
+70 - 79	"C"
+60 - 69	"D"
+0 - 59	"F"*/
+
+
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+console.log(getGrade(100));
+console.log(getGrade(93));
+console.log(getGrade(87));
